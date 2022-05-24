c7cba0bdbffa80050f36312533be4754ca2f72d2
Bianca <you@example.com>
2022-05-24 20:49:07 -0300

    identation adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
-  	namespace 'v1' do
-  		get 'list_managers/:id', to: 'list_managers#list_view'
+    namespace 'v1' do
+      get 'list_managers/:id', to: 'list_managers#list_view'
       post 'list_managers/:id/add_item', to: 'list_managers#add_item'
       delete 'list_managers/:id/remove_item/:id', to: 'list_managers#remove_item'
       patch 'list_managers/:id/items/:id/update_status', to: 'list_managers#update_item_status'
-  	end
+    end
   end
 end
