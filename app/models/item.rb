848e453f10470b4a7213b1546d07a3859bf1be1f
Bianca <you@example.com>
2022-05-24 21:38:44 -0300

    adds model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :to_do_list
 
+  validates :title, uniqueness: { case_sensitive: false, message: "Ooops! Already exists an item with this title" }
+
   enum status: { new_item: 0, read: 1, executed: 2, archived: 3 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_one :to_do_list
+
+  validates :username, uniqueness: true
 end
